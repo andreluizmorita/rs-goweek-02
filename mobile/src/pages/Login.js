d8cee27c21044ec10fd4ff3564c2bd4432958912
andremorita <andreluizmorita@gmail.com>
2018-11-08 20:47:16 -0200

    Estrutura

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
+
+// import styles from './styles';
+
+export default class Login extends Component {
+  render() {
+    return <Text>Login</Text>;
+  }
+}
---
+++ mobile/src/pages/New.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
+
+// import styles from './styles';
+
+export default class New extends Component {
+  render() {
+    return <Text>New</Text>;
+  }
+}
